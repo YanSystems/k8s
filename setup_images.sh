da3b63e9a7844dbaa8af48bae236ffbf49edc3be
abyanmajid <abyan@abydyl.net>
2024-07-13 00:53:03 +0700

    patch: image pull errors and added authorization

--- setup_images.sh
+++ setup_images.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 IMAGES=(
-  "abyanmajid/yan-broker:latest"
-  "abyanmajid/yan-compiler:latest"
-  "abyanmajid/yan-cms:latest"
+	"abyanmajid/yan-broker:latest"
+	"abyanmajid/yan-compiler:latest"
+	"abyanmajid/yan-cms:latest"
 )
 TAGS=(
-  "localhost:5000/compiler"
-  "localhost:5000/cms"
+	"localhost:5000/broker"
+	"localhost:5000/compiler"
+	"localhost:5000/cms"
 )
 
 for IMAGE in "${IMAGES[@]}"; do
-  echo "Pulling image $IMAGE"
-  docker pull $IMAGE
+	echo "Pulling image $IMAGE"
+	docker pull $IMAGE
 done
 
 for i in "${!IMAGES[@]}"; do
-  IMAGE="${IMAGES[$i]}"
-  TAG="${TAGS[$i]}"
-  
-  echo "Retagging image $IMAGE to $TAG"
-  docker tag $IMAGE $TAG
+	IMAGE="${IMAGES[$i]}"
+	TAG="${TAGS[$i]}"
 
-  echo "Pushing image $TAG to local registry"
-  docker push $TAG
+	echo "Retagging image $IMAGE to $TAG"
+	docker tag $IMAGE $TAG
+
+	echo "Pushing image $TAG to local registry"
+	docker push $TAG
 done
 
 echo "All images have been pulled, retagged, and pushed to the local registry."
